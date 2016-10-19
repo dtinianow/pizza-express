5b90defda2e589544c3aa0fd71143fac37697cd8
David Tinianow <david.tinianow@gmail.com>
2016-10-19 11:43:46 -0600

    Complete tutorial

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const generateId = require('./lib/generate-id');
+
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'jade');
+
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
+app.locals.pizzas = {};
 
 app.get('/', (request, response) => {
-  response.send(app.locals.title);
+  response.render('index');
+});
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+
+  response.render('pizza', { pizza: pizza });
+});
+
+app.post('/pizzas', (request, response) => {
+  if (!request.body.pizza) { return response.sendStatus(400); }
+
+  var id = generateId();
+
+  app.locals.pizzas[id] = request.body.pizza;
+
+  response.redirect('/pizzas/' + id);
 });
 
 if (!module.parent) {
